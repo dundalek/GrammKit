980fc36426d3a4d7fb27cb5c94b3b3de998d5df6
Jakub Dundalek <dundalek@gmail.com>
2015-03-11 16:55:52 +0100

    Add build script

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var gulpWebpack = require('gulp-webpack');
+var webpackConfig = require('./webpack.config');
+
+gulp.task('build', ['webpack', 'copy']);
+
+gulp.task('dev', function(callback) {
+  var config = webpackConfig({hot: true});
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+  }).listen(3000, 'localhost', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log('Listening at localhost:3000');
+  });
+});
+
+gulp.task("webpack", function() {
+  var config = webpackConfig();
+  return gulp.src('app/index.js')
+      .pipe(gulpWebpack(config))
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copy', ['copy:files', 'copy:libs']);
+
+gulp.task('copy:libs', function() {
+  return gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css', {base: 'node_modules/bootstrap/dist/css/'})
+    .pipe(gulp.dest('dist/'));
+})
+
+gulp.task('copy:files', function() {
+  return gulp.src('app/{index.html,style.css}', {base: 'app/'})
+    .pipe(gulp.dest('dist/'));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,35 @@
 var webpack = require('webpack');
 
-module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './app/index'
-  ],
-  output: {
-    path: __dirname + '/app/',
-    filename: 'bundle.js',
-    publicPath: '/app/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      { test: /\.jsx?$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ },
-      { test: /\.json$/, loaders: ['json']}
-    ]
-  }
+module.exports = function(options) {
+  options = options || {};
+  return {
+    devtool: 'eval',
+    entry: options.hot ? [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './app/index'
+    ] : ['./app/index'],
+    output: {
+      path: __dirname + '/app/',
+      filename: 'bundle.js',
+      publicPath: '/dist/'
+    },
+    plugins: options.hot ? [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    ] : [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.OccurenceOrderPlugin()
+    ],
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+    module: {
+      loaders: [
+        { test: /\.jsx?$/, loaders: options.hot ? ['react-hot', 'jsx?harmony'] : ['jsx?harmony'], exclude: /node_modules/ },
+        { test: /\.json$/, loaders: ['json']}
+      ]
+    }
+  };
 };
