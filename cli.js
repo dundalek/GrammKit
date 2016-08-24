94e6bad7b0cf07c3a1f2e594e784c379288c4d53
sramam <sramam@gmail.com>
2016-08-24 02:22:51 -0700

    Adding a cli to generate a self contained html viewer or svg

--- cli.js
+++ cli.js
@@ -8,26 +8,26 @@ var handlebars = require('handlebars');
 
 var parse = require('pegjs/lib/parser').parse;
 var grammkit = require('./grammkit');
+var getReferences = require('./lib/peg-references');
 
 var version = require('./package.json').version;
 
-var getHtml = function (svg,title) {
+var genHtml = function (title, rules) {
 	var raw_template = fs.readFileSync(path.join(__dirname, 'template', 'viewer.html'));
 	var data = {
 		title: title,
 		style: fs.readFileSync(path.join(__dirname, 'template','style.css'),  'utf-8'),
-    svg: svg
+		rules: rules
 	};
 	var template = handlebars.compile(raw_template.toString());
 	return template(data);
 }
 
+
 program
 	.version(version)
   .usage('[options] <grammer_file>')
 	.option('-o, --output <output>', 'output file', null)
-	.option('-g, --grammer <format>', '"pegjs" or "ebnf". Defaults to pegjs', 'pegjs')
-  .option('--html', 'generate html file for viewing in a browser')
   .parse(process.argv);
 
 var input = program.args[0] || null;
@@ -43,16 +43,23 @@ var title = p.base;
 
 if (program.output === null) {
 	p.dir = '.';
-	p.ext = program.html ? '.html':'.svg';
+	p.ext = '.html';
 	delete p.base;
 	program.output = path.format(p);
 }
 
 var grammar = parse(fs.readFileSync(input, 'utf-8'));
-var svg = grammkit.diagram(grammar.rules[0]);
+var references = getReferences(grammar);
+var rules = grammar.rules.map(function(rule) {
+	return {
+		name: rule.name,
+		diagram: grammkit.diagram(rule),
+		usedBy: references[rule.name].usedBy,
+		references: references[rule.name].references
+	};
+});
 
-if (program.html) {
-	fs.writeFileSync(program.output, getHtml(svg, title), 'utf-8');	
-} else {
-	fs.writeFileSync(program.output, svg, 'utf-8');
-}
+var html = genHtml(title, rules)
+
+fs.writeFileSync(program.output, html, 'utf-8');
+console.log('generated ' + program.output);
