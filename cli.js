9097aa0fa91feb2a7acb3ccd406f32b27f14b60b
sramam <sramam@gmail.com>
2016-08-24 15:57:58 -0700

    fixing generation for production rules that have no references and/or are not referenced by any other rules

--- cli.js
+++ cli.js
@@ -12,51 +12,54 @@ var getReferences = require('./lib/peg-references');
 
 var version = require('./package.json').version;
 
-var genHtml = function (title, rules) {
-	var raw_template = fs.readFileSync(path.join(__dirname, 'template', 'viewer.html'));
-	var data = {
-		title: title,
-		style: fs.readFileSync(path.join(__dirname, 'template','style.css'),  'utf-8'),
-		rules: rules
-	};
-	var template = handlebars.compile(raw_template.toString());
-	return template(data);
+var genHtml = function(title, rules) {
+    var raw_template = fs.readFileSync(path.join(__dirname, 'template', 'viewer.html'));
+    var data = {
+        title: title,
+        style: fs.readFileSync(path.join(__dirname, 'template', 'style.css'), 'utf-8'),
+        rules: rules
+    };
+    var template = handlebars.compile(raw_template.toString());
+    return template(data);
 }
 
 
 program
-	.version(version)
-  .usage('[options] <grammer_file>')
-	.option('-o, --output <output>', 'output file', null)
-  .parse(process.argv);
+    .version(version)
+    .usage('[options] <grammer_file>')
+    .option('-o, --output <output>', 'output file', null)
+    .parse(process.argv);
 
 var input = program.args[0] || null;
 
 if (!input) {
-	console.error('ERROR: Missing a required input grammer file.');
-	console.log(program.help());
-	process.exit(1)
+    console.error('ERROR: Missing a required input grammer file.');
+    console.log(program.help());
+    process.exit(1)
 }
 
 var p = path.parse(input);
 var title = p.base;
 
 if (program.output === null) {
-	p.dir = '.';
-	p.ext = '.html';
-	delete p.base;
-	program.output = path.format(p);
+    p.dir = '.';
+    p.ext = '.html';
+    delete p.base;
+    program.output = path.format(p);
 }
 
 var grammar = parse(fs.readFileSync(input, 'utf-8'));
-var references = getReferences(grammar);
+var References = getReferences(grammar);
 var rules = grammar.rules.map(function(rule) {
-	return {
-		name: rule.name,
-		diagram: grammkit.diagram(rule),
-		usedBy: references[rule.name].usedBy,
-		references: references[rule.name].references
-	};
+    const _ref = References[rule.name] || null;
+    const usedBy = (_ref && _ref.usedBy) || null;
+    const references = (_ref && _ref.references) || null;
+    return {
+        name: rule.name,
+        diagram: grammkit.diagram(rule),
+        usedBy: usedBy,
+        references: references
+    };
 });
 
 var html = genHtml(title, rules)
